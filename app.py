031c44d50d91829b6e618be2a205685c7661afb8
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-08 21:36:51 -0500

    user cn register

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, jsonify
 from resources.users import users
-
+from flask_login import LoginManager, current_user
+import models
 DEBUG=True
 PORT=8000
 
 app= Flask(__name__)
 
+app.secret_key="a secret"
+login_manager= LoginManager()
+login_manager.init_app(app) 
+
+app.register_blueprint(users, url_prefix='/api/v1/users')
+
 @app.route('/')
 def hello():
 	return "hell world"
-app.register_blueprint(users, url_prefix='/api/v1/users')
 
 if __name__ == '__main__':
+	models.initialize()
 	app.run(debug=DEBUG, port=PORT)
 
 
--- resources/users.py
+++ resources/users.py
@@ -10,3 +10,50 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=["GET"])
 def test():
 	return "user resourse hello"
+
+@users.route('/register', methods=['POST'])
+def register():
+	payload=request.get_json()
+	print(payload)
+
+	payload['email']=payload['email'].lower()
+	payload['username'] = payload['email'].lower()
+
+	try:
+		models.User.get(models.User.email == payload['email'])
+		return jsonify(
+			data={},
+			message="That email is already in our system",
+			status=401
+			), 401
+	except models.DoesNotExist:
+
+		try:
+			models.User.get(models.User.username == payload['username'])
+			return jsonify(
+				data={},
+				message="That username isn't available",
+				status=401), 401
+		except models.DoesNotExist:
+
+			pw_hash = generate_password_hash(payload['password'])
+			created_user = models.User.create(
+				username=payload['username'],
+				email=payload['email'],
+				password=pw_hash,
+				zipcode=payload['zipcode'],
+				DOB=payload['DOB']
+			)
+			login_user(created_user)
+			created_user_dict = model_to_dict(created_user)
+			created_user_dict.pop('password')
+
+			return jsonify(
+				data=created_user_dict,
+				message="registerd user",
+				status=201), 201
+
+
+
+
+
