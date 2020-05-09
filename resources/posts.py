d9470cb4131ac39202c07d0938f2cae76254dfef
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-08 22:42:50 -0500

    posts index works

--- resources/posts.py
+++ resources/posts.py
@@ -6,15 +6,16 @@ from playhouse.shortcuts import model_to_dict
 from flask_login import LoginManager, current_user
 posts = Blueprint('posts', 'posts')
 
-@posts.route('/', methods=["GET"])
-def test():
-	return "posts resource"
+# @posts.route('/', methods=["GET"])
+# def test():
+# 	return "posts resource"
 
 @posts.route('/', methods=["POST"])
 def create_post():
 	payload = request.get_json()
 	print('this is the payload')
 	print(payload)
+
 	new_post = models.Post.create(text=payload['text'], image=payload['image'],Latitude=payload['Latitude'], Longitude=payload["Longitude"], user=current_user.id)
 	print(new_post)
 	post_dict = model_to_dict(new_post)
@@ -24,3 +25,13 @@ def create_post():
 		message="succesfully created a post",
 		status=200), 200
 
+@posts.route('/', methods=["GET"])
+def posts_index():
+	posts = models.Post.select()
+	print(posts)
+	posts_dict = [model_to_dict(post) for post in posts]
+
+	return jsonify(
+		data=posts_dict,
+		message="found all of the posts",
+		status=200), 200
